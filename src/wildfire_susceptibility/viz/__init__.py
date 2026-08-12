b4ffd191cfc663b0d1ad1a169d219b7a55bce27b
Virtual Self <a01722401@tec.mx>
2026-08-12 09:21:57 -0600

    fixed conflict branches 2

--- src/wildfire_susceptibility/viz/__init__.py
+++ src/wildfire_susceptibility/viz/__init__.py
@@ -1,5 +1,10 @@
-from .charts import plot_vif_correlation, plot_class_balance, plot_nan_coverage, plot_roc_curves, plot_shap_summary, plot_cv_comparison, plot_temporal_decomposition, plot_acf_pacf, plot_spatial_correlogram, plot_confusion_matrix
-from .charts import plot_vif_correlation, plot_class_balance, plot_nan_coverage, plot_roc_curves, plot_shap_summary, plot_cv_comparison, plot_optuna_optimization_history, plot_optuna_param_importances, plot_confusion_matrix, plot_shap_dependence
+from .charts import (
+    plot_vif_correlation, plot_class_balance, plot_nan_coverage, 
+    plot_roc_curves, plot_shap_summary, plot_cv_comparison, 
+    plot_temporal_decomposition, plot_acf_pacf, plot_spatial_correlogram, 
+    plot_confusion_matrix, plot_optuna_optimization_history, 
+    plot_optuna_param_importances, plot_shap_dependence
+)
 from .maps import render_factor_map, render_susceptibility_map, render_all_factor_maps
 from .terrain_overlay import save_terrain_map
 
@@ -9,7 +14,6 @@ __all__ = [
     "plot_nan_coverage",
     "plot_roc_curves",
     "plot_shap_summary",
-    "plot_cv_comparison",
     "plot_temporal_decomposition",
     "plot_acf_pacf",
     "plot_spatial_correlogram",
